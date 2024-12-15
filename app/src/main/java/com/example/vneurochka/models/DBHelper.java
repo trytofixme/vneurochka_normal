70623f11d99e9824edf358050d8e564cf2c931c6
trytofixme <semenov.nikita@phystech.edu>
2024-12-15 15:15:39 +0300

    update

--- app/src/main/java/com/example/vneurochka/models/DBHelper.java
+++ app/src/main/java/com/example/vneurochka/models/DBHelper.java
@@ -1,10 +1,12 @@
 package com.example.vneurochka.models;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+import android.widget.SimpleCursorAdapter;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -12,45 +14,57 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 public class DBHelper extends SQLiteOpenHelper {
-    @Override
-    public void onCreate(SQLiteDatabase db) {}
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion,  int newVersion) { }
-
     static final String DATABASE_NAME = "vneurochka.db";
-    static String DATABASE_PATH = "C:\\Users\\Admin\\AndroidStudioProjects\\vneurochka\\" + DATABASE_NAME;
+    static String DATABASE_PATH;
     static final String USERS_TABLE_NAME = "users";
     static final String GROUP_TYPE_TABLE_NAME = "group_type";
     static final String GROUPS_TABLE_NAME = "groups";
     static final String CHANNEL_HISTORY_TABLE_NAME = "channel_history";
     static final String CHAT_HISTORY_TABLE_NAME = "chat_history";
 
-    private Context context;
+    private final Context context;
+
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
         this.context = context;
+        DATABASE_PATH = context.getFilesDir().getPath() + DATABASE_NAME;
     }
 
-     public void create_db(){
-         File file = new File(DATABASE_PATH);
-         if (!file.exists()) {
-             try(InputStream myInput = context.getAssets().open(DATABASE_PATH);
-                 OutputStream myOutput = new FileOutputStream(DATABASE_PATH)) {
-                 byte[] buffer = new byte[1024];
-                 int length;
-                 while ((length = myInput.read(buffer)) > 0) {
-                     myOutput.write(buffer, 0, length);
-                 }
-                 myOutput.flush();
-             }
-             catch(IOException ex){
-                 Log.d("DatabaseHelper", ex.getMessage());
-             }
-         }
-     }
-     public SQLiteDatabase open()throws SQLException {
-         SQLiteDatabase db = SQLiteDatabase.openDatabase(DATABASE_PATH, null, SQLiteDatabase.OPEN_READWRITE);
-         return db;
-     }
+    @Override
+    public void onCreate(SQLiteDatabase db) { }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion,  int newVersion) { }
+
+    public void create_db() throws IOException {
+
+        File file = new File(DATABASE_PATH);
+        if (!file.exists()) {
+            try(InputStream myInput = context.getAssets().open(DATABASE_NAME);
+                OutputStream myOutput = new FileOutputStream(DATABASE_PATH)) {
+                byte[] buffer = new byte[1024];
+                int length;
+                while ((length = myInput.read(buffer)) > 0) {
+                    myOutput.write(buffer, 0, length);
+                }
+                myOutput.flush();
+            }
+            catch(IOException ex){
+                throw new IOException(ex.getCause());
+            }
+        }
+    }
+    public SQLiteDatabase open()throws SQLException {
+
+        return SQLiteDatabase.openDatabase(DATABASE_PATH, null, SQLiteDatabase.OPEN_READWRITE);
+    }
+
+    public void getUserByLogin(String login) {
+        SQLiteDatabase db = this.open();
+        Cursor userCursor = db.rawQuery("select login, password from users where login =?", new String[]{login});
+        String[] columns = new String[]{"login", "password"};
+        SimpleCursorAdapter userAdapter = new SimpleCursorAdapter(context, android.R.layout.two_line_list_item,
+                userCursor, columns, new int[]{android.R.id.text1, android.R.id.text2}, 0);
+        userAdapter.getFilter().filter(login);
+    }
 }
--- app/src/main/java/com/example/vneurochka/pages/RegistrationActivity.java
+++ app/src/main/java/com/example/vneurochka/pages/RegistrationActivity.java
@@ -1,16 +1,9 @@
 package com.example.vneurochka.pages;
 
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.SimpleAdapter;
-import android.widget.SimpleCursorAdapter;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,87 +16,66 @@ import com.example.vneurochka.models.UserViewModel;
 import java.io.IOException;
 
 public class RegistrationActivity extends AppCompatActivity {
-    SimpleCursorAdapter userAdapter;
     DBHelper dbHelper;
-    SQLiteDatabase db;
-    private final UserViewModel userViewModel;
     public RegistrationActivity() {
-        this.userViewModel = new UserViewModel();
-    }
 
-    public EditText login, password, rpt_password, e_mail;
-    public Button btn;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         try {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.registration_activity);
-            login = findViewById(R.id.login);
-            password = findViewById(R.id.password); //Rezinka007
-            rpt_password = findViewById(R.id.rpt_password);
-            e_mail = findViewById(R.id.e_mail);
-            btn = findViewById(R.id.btn);
+
+            String loginText = ((EditText)findViewById(R.id.login)).getText().toString();
+            String passwordText = ((EditText)findViewById(R.id.password)).getText().toString(); //Rezinka007
+            String rptPasswordText = ((EditText)findViewById(R.id.rpt_password)).getText().toString();
+            String mailText = ((EditText)findViewById(R.id.e_mail)).getText().toString();
+            Button registrationButton = findViewById(R.id.btn);
+
             dbHelper = new DBHelper(this);
-            db = dbHelper.getWritableDatabase();
-            Cursor cr_user = db.rawQuery("select login, password from users where login =?", new String[]{login.getText().toString()});
-            String[] columns = new String[]{"login", "password"};
-            userAdapter = new SimpleCursorAdapter(this, android.R.layout.two_line_list_item, cr_user, columns, new int[]{android.R.id.text1, android.R.id.text2}, 0);
-            userAdapter.getFilter().filter(login.getText().toString());
-            btn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if ((Character.isDigit(password.getText().toString().charAt(0))) || (password.getText().toString().length() < 7)) {
-                        AlertDialog.Builder builder = new AlertDialog.Builder(RegistrationActivity.this);
-                        builder.setTitle(R.string.error_password_title)
-                                .setMessage(R.string.error_password_text)
-                                .setCancelable(false)
-                                .setPositiveButton(R.string.error_password_pb, new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        dialog.cancel();
-                                    }
-                                });
-                        AlertDialog dialog = builder.create();
-                        dialog.show();
-                    } else if ((Character.isDigit(login.getText().toString().charAt(0))) || (login.getText().toString().length() < 4)) {
-                        AlertDialog.Builder builder = new AlertDialog.Builder(RegistrationActivity.this);
-                        builder.setTitle(R.string.error_login_title)
-                                .setMessage(R.string.error_login_text)
-                                .setCancelable(false)
-                                .setPositiveButton(R.string.error_login_pb, new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        dialog.cancel();
-                                    }
-                                });
-                        AlertDialog dialog = builder.create();
-                        dialog.show();
-                    } else if (password.getText().toString() == rpt_password.getText().toString()) {
-                        AlertDialog.Builder builder = new AlertDialog.Builder(RegistrationActivity.this);
-                        builder.setTitle(R.string.error_rpt_title)
-                                .setMessage(R.string.error_rpt_text)
-                                .setCancelable(false)
-                                .setPositiveButton(R.string.error_rpt_pb, new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        dialog.cancel();
-                                    }
-                                });
-                        AlertDialog dialog = builder.create();
-                        dialog.show();
-                    } else {
-                        User user = new User(login.getText().toString(), password.getText().toString(), e_mail.getText().toString());
-                        userViewModel.addUser(user);
-                        Intent intent = new Intent(RegistrationActivity.this, AuthorizationActivity.class);
-                        intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-                        startActivity(intent);
-                    }
+            dbHelper.create_db();
+
+            registrationButton.setOnClickListener(v -> {
+                // Password validation
+                if ((Character.isDigit(passwordText.charAt(0))) || (passwordText.length() < 7)) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(RegistrationActivity.this);
+                    builder.setTitle(R.string.error_password_title)
+                        .setMessage(R.string.error_password_text)
+                        .setCancelable(false)
+                        .setPositiveButton(R.string.error_password_pb, (dialog, which) -> dialog.cancel());
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                // Login validation
+                } else if ((Character.isDigit(loginText.charAt(0))) || (loginText.length() < 4)) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(RegistrationActivity.this);
+                    builder.setTitle(R.string.error_login_title)
+                        .setMessage(R.string.error_login_text)
+                        .setCancelable(false)
+                        .setPositiveButton(R.string.error_login_pb, (dialog, which) -> dialog.cancel());
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                // Validating password matching
+                } else if (passwordText.equals(rptPasswordText)) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(RegistrationActivity.this);
+                    builder.setTitle(R.string.error_rpt_title)
+                        .setMessage(R.string.error_rpt_text)
+                        .setCancelable(false)
+                        .setPositiveButton(R.string.error_rpt_pb, (dialog, which) -> dialog.cancel());
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                } else {
+                    User user = new User(loginText, passwordText, mailText);
+                    UserViewModel.addUser(user);
+                    Intent intent = new Intent(RegistrationActivity.this, AuthorizationActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                    startActivity(intent);
                 }
             });
-        } catch (Exception ex) {
-            Log.d("DatabaseHelper", ex.getMessage());
         }
-
+        catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
     }
 }
